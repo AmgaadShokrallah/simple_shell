ddd2c99d7334d5902d9fa76e1d8a22a4eb85c04d
AmgaadShokrallah <amgaad@example.com>
2023-10-18 03:58:20 -0700

    commit

--- more_function.c
+++ more_function.c
@@ -7,19 +7,20 @@
 char *rmcommand(char *command)
 {
 	char *rmcommand;
-	int i = 0, len;
+	int lenght;
+	int i = 0;
 
 	while (command[i] == ' ')
 		i++;
-	len = _strlen(command) - i + 1;
-	rmcommand = malloc(len + 1);
-	if (!rmcommand)
+	lenght = _strlen(command) - i + 1;
+	rmcommand = malloc(1 + lenght);
+	if (rmcommand == NULL)
 	{
 		perror("Failed\n");
 		return (NULL);
 	}
-	_strncpy(rmcommand, command + i, len);
-	rmcommand[len] = '\0';
+	_strncpy(rmcommand, command + i, lenght);
+	rmcommand[lenght] = '\0';
 	_strcpy(command, rmcommand);
 	free(rmcommand);
 	return (command);
